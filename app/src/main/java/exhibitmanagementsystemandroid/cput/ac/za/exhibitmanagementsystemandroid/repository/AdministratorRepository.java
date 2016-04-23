41c331e247bd46a8fab848e09ae13a8074fe657b
202136272 <bmabulu@webmail.co.za>
2016-04-22 21:14:34 -0700

    Crud Insert 9pm

---
+++ app/src/main/java/exhibitmanagementsystemandroid/cput/ac/za/exhibitmanagementsystemandroid/repository/AdministratorRepository.java
@@ -0,0 +1,10 @@
+package exhibitmanagementsystemandroid.cput.ac.za.exhibitmanagementsystemandroid.repository;
+
+import exhibitmanagementsystemandroid.cput.ac.za.exhibitmanagementsystemandroid.domain.Administrator;
+
+/**
+ * Created by Bonga on 4/22/2016.
+ */
+public interface AdministratorRepository extends Repository<Administrator,Long>{
+
+}
